8e840e23b6bc338250d8edc8879a80cef7214078
Justas Krikščiūnas <justas.kriksciunas@estotyvilnius.com>
2023-03-06 22:57:25 +0200

    Added effection functionality to sink and trashcan

--- Assets/Scripts/Sink.cs
+++ Assets/Scripts/Sink.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Sink : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem cleanVFX;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<PlateStates>() != null)
         {
             other.gameObject.GetComponent<PlateStates>().Clean();
+            cleanVFX.Play();
         }
     }
 }
--- Assets/Scripts/TrashCan.cs
+++ Assets/Scripts/TrashCan.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class TrashCan : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem trashVFX;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<IngredientParam>() != null)
         {
             if (other.gameObject.GetComponent<IngredientParam>().canDelete == true)
+            {
                 GameObject.Destroy(other.gameObject);
+                trashVFX.Play();
+            }
         }
     }
 }
